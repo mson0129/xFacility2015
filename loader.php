2f414b2a5f80ee2c0121ee9aeef5a321886871ab
Michael Son <mson0129@gmail.com>
2015-04-27 01:04:20 +0900

    Stopping Error with a non-exist path
    There is an error that can make the loader stopped by a non-exist path.
    The file was corrected to ignore a non-exist path.

--- loader.php
+++ loader.php
@@ -5,6 +5,7 @@
 //Michael Son(mson0129@gmail.com)
 //22SEP2014(1.0.0.) - Loader is updated.
 //16MAR2015(1.1.0.) - Loader is divided from XFObject.class.php of xFacility2014. And path setting is modified. It's set automatically.
+//27APR2015(1.1.1.) - Stopping with a non-exist path is corrected.
 
 function load($classPath=NULL) {
 	if(!is_null($classPath)) {
@@ -17,19 +18,21 @@ function load($classPath=NULL) {
 	$j = 1;
 	for($i=0; $i<$j; $i++) {
 		$handle = opendir($paths[$i]);
-		while(false !==($file = readdir($handle))) {
-			if ($file != "." && $file != "..") {
-				if(!is_dir($paths[$i]."/".$file)) {
-					if(substr($file, 0, 1)!=".") {
-						if($_GET['debug']==true)
-							echo $paths[$i]."/".$file;
-						require_once($paths[$i]."/".$file);
-						if($_GET['debug']==true)
-							echo "...OK\n";
+		if(is_dir($paths[$i])) {
+			while(false !==($file = readdir($handle))) {
+				if ($file != "." && $file != "..") {
+					if(!is_dir($paths[$i]."/".$file)) {
+						if(substr($file, 0, 1)!=".") {
+							if($_GET['debug']==true)
+								echo $paths[$i]."/".$file;
+							require_once($paths[$i]."/".$file);
+							if($_GET['debug']==true)
+								echo "...OK\n";
+						}
+					} else {
+						$paths[] = $paths[$i]."/".$file;
+						$j = count($paths);
 					}
-				} else {
-					$paths[] = $paths[$i]."/".$file;
-					$j = count($paths);
 				}
 			}
 		}
